c43a354e1eaad6eb22fe8078e533d7d992c4728d
“evelinecai” <r.t.cai@tilburguniversity.edu>
2024-10-11 18:26:38 +0200

    alles klopt weer, files in dataprep weggehaald

--- src/data-preparation/remove_variables.R
+++
@@ -1,9 +0,0 @@
-Following these adjustments to the datasets, we also removed unnecessary columns that contained information deemed irrelevant to the scope of this research.
-
-```{r}
-# removal of unnecessary columns
-library(dplyr)
-cdata1 <- data1 %>% select(primaryName, primaryProfession, knownForTitles)
-cdata2 <- data2
-cdata3 <- data3 %>% select(tconst, primaryTitle, startYear, genres)
-```
